897881b3d352f622d6169fc2079c213cd431422b
Richard Willis <willis.rh@gmail.com>
2012-10-26 00:24:39 +0100

    Replaced XRegExp library with character sets to save some bytes; added more tests for text parser

--- test/spec/spellchecker.js
+++ test/spec/spellchecker.js
@@ -10,10 +10,13 @@ describe("SpellChecker", function() {
 
   describe('Text parser', function() {
 
-    it('Removes punctuation from text', function() {
+    var spellchecker = new $.SpellChecker(null, {
+      parser: 'text'
+    });
 
-      var spellchecker = new $.SpellChecker(null);
-      var parser = spellchecker.parser;
+    var parser = spellchecker.parser;
+
+    it('Removes punctuation from text', function() {
 
       var text1 = 'Hello, this is a-test. How are you today?';
       var text2 = '  ...Hello, here\'s a-test. How are you today?';
@@ -26,9 +29,6 @@ describe("SpellChecker", function() {
 
     it('Removes numbers from text', function() {
 
-      var spellchecker = new $.SpellChecker(null);
-      var parser = spellchecker.parser;
-
       var text1 = 'Hello, 123, this is a-test. \'456\' How are you today?';
       var cleaned1 = parser.clean(text1);
 
@@ -37,41 +37,70 @@ describe("SpellChecker", function() {
 
     it('Removes punctuation and numbers from Unicode text', function() {
 
-      var spellchecker = new $.SpellChecker(null);
-      var parser = spellchecker.parser;
+      var tests = {
+         // greek
+        'Γεια, αυτό είναι ένα-δοκιμή. Πώς είστε σήμερα;': 
+        'Γεια αυτό είναι ένα-δοκιμή Πώς είστε σήμερα',
+         // russian
+        'Здравствуйте, это-тест. Как вы сегодня?': 
+        'Здравствуйте это-тест Как вы сегодня',
+         // arabic
+        'مرحبا، وهذا هو الاختبار. كيف حالك اليوم؟': 
+        'مرحبا وهذا هو الاختبار كيف حالك اليوم',
+         // hebrew
+        'הלו, זה מבחן. מה שלומך היום?': 
+        'הלו זה מבחן מה שלומך היום',
+         // spanish
+        'Hola, esta es una prueba. ¿Cómo estás hoy?': 
+        'Hola esta es una prueba Cómo estás hoy',
+         // chinese (simplified)
+        '你好，这是一个测试。你今天怎么样呢？': 
+        '你好，这是一个测试。你今天怎么样呢',
+         // french
+        'Bonjour, ceci est un test-. Comment allez-vous aujourd\'hui?': 
+        'Bonjour ceci est un test Comment allez-vous aujourd\'hui',
+         // random
+        'Aynı, labda - çalıştığım? \"quote\". Föö bär, we\'re @test to0 ÅÄÖ - 123 ok? kthxbai?': 
+        'Aynı labda çalıştığım quote Föö bär we\'re test to0 ÅÄÖ ok kthxbai',
+         // hindi
+        'नमस्कार, यह एक परीक्षण है. आज तुम कैसे हो?': 
+        'नमस्कार यह एक परीक्षण है आज तुम कैसे हो',
+         // hungarian
+        'Halló, ez egy-teszt. Hogy van ma?': 
+        'Halló ez egy-teszt Hogy van ma',
+         // japanese
+        'こんにちは、これは、テストです。元気ですか？': 
+        'こんにちは、これは、テストです。元気ですか',
+         // finnish
+        'Hei, tämä on-testi. Miten voit tänään?': 
+        'Hei tämä on-testi Miten voit tänään',
+         // italian
+        'Ciao, questo è un test. Come stai oggi?': 
+        'Ciao questo è un test Come stai oggi',
+         // korean
+        '안녕하세요 테스트입니다. 안녕하세요?': 
+        '안녕하세요 테스트입니다 안녕하세요',
+         // portuguese
+        'Olá, este é um teste. Como você está hoje?': 
+        'Olá este é um teste Como você está hoje'
+      };
 
-      var text1 = 'Γεια, αυτό είναι ένα-δοκιμή. Πώς είστε σήμερα;'; // greek
-      var text2 = 'Здравствуйте, это-тест. Как вы сегодня?'; // russian
-      var text3 = 'مرحبا، وهذا هو الاختبار. كيف حالك اليوم؟'; // arabic
-      var text4 = 'הלו, זה מבחן. מה שלומך היום?'; // hebrew
-      var text5 = 'Hola, esta es una prueba. ¿Cómo estás hoy?'; // spanish
-      var text6 = '你好，这是一个测试。你今天怎么样呢？'; // chinese (simplified)
-      var text7 = 'Bonjour, ceci est un test-. Comment allez-vous aujourd\'hui?'; // french
-      var text8 = 'Aynı, labda - çalıştığım? \"quote\". Föö bär, we\'re @test to0 ÅÄÖ - 123 ok? kthxbai?'; // random
-      var text9 = 'नमस्कार, यह एक परीक्षण है. आज तुम कैसे हो?'; // hindi
-      var text10 = 'Halló, ez egy-teszt. Hogy van ma?'; // hungarian
+      for(var key in tests) {
+        expect(parser.clean(key)).toBe(tests[key]);
+      }
+    });
 
-      var cleaned1 = parser.clean(text1);
-      var cleaned2 = parser.clean(text2);
-      var cleaned3 = parser.clean(text3);
-      var cleaned4 = parser.clean(text4);
-      var cleaned5 = parser.clean(text5);
-      var cleaned6 = parser.clean(text6);
-      var cleaned7 = parser.clean(text7);
-      var cleaned8 = parser.clean(text8);
-      var cleaned9 = parser.clean(text9);
-      var cleaned10 = parser.clean(text10);
+    it('Replaces a word multiple times in a string of text', function() {
+      var text = 'Hello, are you ok? Would you like some coke? No thanks, I am ok!';
+      var replaced = parser.replaceWordInText(text, 'ok', 'good');
+      expect(replaced).toBe('Hello, are you good? Would you like some coke? No thanks, I am good!');
+    });
 
-      expect(cleaned1).toBe('Γεια αυτό είναι ένα-δοκιμή Πώς είστε σήμερα');
-      expect(cleaned2).toBe('Здравствуйте это-тест Как вы сегодня');
-      expect(cleaned3).toBe('مرحبا وهذا هو الاختبار كيف حالك اليوم');
-      expect(cleaned4).toBe('הלו זה מבחן מה שלומך היום');
-      expect(cleaned5).toBe('Hola esta es una prueba Cómo estás hoy');
-      expect(cleaned6).toBe('你好，这是一个测试。你今天怎么样呢');  // FIXME: is this correct?
-      expect(cleaned7).toBe('Bonjour ceci est un test Comment allez-vous aujourd\'hui');
-      expect(cleaned8).toBe('Aynı labda çalıştığım quote Föö bär we\'re test to0 ÅÄÖ ok kthxbai');
-      expect(cleaned9).toBe('नमस्कार यह एक परीक्षण है आज तुम कैसे हो');
-      expect(cleaned10).toBe('Halló ez egy-teszt Hogy van ma');
+    it('Replaces a Unicode word multiple times in a string of text', function() {
+      var text = 'Привет, ты в порядке? Хотели бы Вы немного кокса? Нет, спасибо, я в порядке!';
+      var replaced = parser.replaceWordInText(text, 'порядке', 'хорошо');
+      console.log(replaced);
+      expect(replaced).toBe('Привет, ты в хорошо? Хотели бы Вы немного кокса? Нет, спасибо, я в хорошо!');
     });
   });  
 });
